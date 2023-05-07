d3e4f943bc195bf53376e3be244570fbc17087c7
Avi Vahl <avi.vahl@wix.com>
2023-05-07 18:17:09 +0300

    infra: print dist gzip sizes

---
+++ scripts/print-sizes.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+import fs from "node:fs";
+import { promisify } from "node:util";
+import { deflate as deflateCb } from "node:zlib";
+const deflate = promisify(deflateCb);
+
+const bytesInKB = 1024;
+const bytesInMB = bytesInKB * 1024;
+const distURL = new URL("../dist/", import.meta.url);
+console.log("<name>".padStart(30))
+await printDirectoryDeep("dist", distURL);
+
+async function printDirectoryDeep(directoryName, directoryURL, padding = 0) {
+  console.log(`${directoryName}/`.padStart(padding + directoryName.length + 1));
+  for (const item of fs.readdirSync(directoryURL, { withFileTypes: true })) {
+    if (item.isFile()) {
+      const fileName = item.name;
+      const fileURL = new URL(fileName, directoryURL);
+      const { compressed, raw } = await fileSizes(fileURL);
+      const saved = `${(100 - (compressed / raw) * 100).toFixed(0)}%`;
+
+      const firstColumn = fileName.padStart(padding + 2 + fileName.length).padEnd(28 - padding);
+      const secondColumn = humanReadableSize(raw).padEnd(10);
+      const thirdColumn = humanReadableSize(compressed).padEnd(10) + ` (${saved} saved)`;
+
+      console.log(`${firstColumn}${secondColumn} -> ${thirdColumn}`);
+    } else if (item.isDirectory()) {
+      await printDirectoryDeep(item.name, new URL(`${item.name}/`, directoryURL), padding + 2);
+    }
+  }
+}
+
+async function fileSizes(fileURL) {
+  const uncompressedBuffer = fs.readFileSync(fileURL);
+  const compressedBuffer = await deflate(uncompressedBuffer);
+  return { raw: uncompressedBuffer.byteLength, compressed: compressedBuffer.byteLength };
+}
+
+function humanReadableSize(bytes) {
+  if (bytes >= bytesInMB) {
+    return `${(bytes / bytesInMB).toFixed(2)} MB`;
+  } else if (bytes >= bytesInKB) {
+    return `${(bytes / bytesInKB).toFixed(2)} KB`;
+  } else {
+    return `${bytes} bytes`;
+  }
+}
