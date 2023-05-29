52d90045d6b5762c9d0f4e41d7184ce637c423a2
Avi Vahl <avi.vahl@wix.com>
2023-05-29 23:51:32 +0300

    fix: compat with older sass versions older versions did not set the exports on the passed objeect, but on the exports object itself. use that if populated

--- src/compilation/compilation-libs.ts
+++ src/compilation/compilation-libs.ts
@@ -5,12 +5,33 @@ export function evaluateSassLib(sassURL: string, sassLibText: string, immutableL
   const immutable = immutableJsModuleSystem.requireModule(immutableURL) as typeof import("immutable");
 
   const sassModuleSystem = singlePackageModuleSystem("sass", sassURL, sassLibText);
-  const { load } = sassModuleSystem.requireModule(sassURL) as {
+
+  sassModuleSystem.requireCache.set("util", {
+    id: "util",
+    filename: "util",
+    exports: { inspect: {} },
+    children: [],
+  });
+  sassModuleSystem.requireCache.set("fs", {
+    id: "fs",
+    filename: "fs",
+    exports: {},
+    children: [],
+  });
+  sassModuleSystem.requireCache.set("immutable", {
+    id: "immutable",
+    filename: "immutable",
+    exports: immutable,
+    children: [],
+  });
+
+  const sassExports = sassModuleSystem.requireModule(sassURL) as {
     load: (deps: { util: unknown; immutable: unknown }, target: unknown) => void;
   };
+
   const sassLib = {} as unknown as typeof import("sass");
-  load({ util: { inspect: {} }, immutable }, sassLib);
-  return sassLib;
+  sassExports.load({ util: { inspect: {} }, immutable }, sassLib);
+  return "info" in sassLib ? sassLib : (sassExports as unknown as typeof import("sass"));
 }
 
 export function evaluateTypescriptLib(typescriptURL: string, typescriptText: string) {
