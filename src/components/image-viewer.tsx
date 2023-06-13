4497a33419e122abe1b019a55ab5cafeddbae339
Barak Igal <baraki@wix.com>
2023-06-14 01:30:54 +0300

    feat: improve image viewer

--- src/components/image-viewer.tsx
+++ src/components/image-viewer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./image-viewer.module.css";
 import { classNames } from "../helpers/dom";
 
@@ -11,7 +11,29 @@ export namespace ImageViewer {
 }
 
 export const ImageViewer: React.FC<ImageViewer.Props> = React.memo(({ className, imageUrl, filePath }) => {
-  return <img className={classNames(className, classes["imageViewer"])} src={imageUrl} title={filePath} />;
+  const [zoom, setZoom] = useState(1);
+
+  const handleZoom = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setZoom(Math.pow(10, e.currentTarget.valueAsNumber * 2 - 1));
+  };
+
+  return (
+    <div className={classNames(className, classes["imageViewer"])}>
+      <div className={classes["controls"]}>
+        <input defaultValue={0.5} type="range" min="0" max="1" step="0.01" onChange={handleZoom} />
+      </div>
+      <div className={classes["stage"]}>
+        <img
+          className={classNames(classes["view"], classes["checkersBg"])}
+          src={imageUrl}
+          title={filePath}
+          style={{
+            transform: `scale(${zoom})`,
+          }}
+        />
+      </div>
+    </div>
+  );
 });
 
 ImageViewer.displayName = "ImageViewer";
