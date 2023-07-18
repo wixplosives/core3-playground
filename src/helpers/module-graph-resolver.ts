97cb5cde2c093f5013bf26def017fe7f09c989a9
Avi Vahl <avi.vahl@wix.com>
2023-07-19 00:11:47 +0300

    perf: parallel module graph resolution

--- src/helpers/module-graph-resolver.ts
+++ src/helpers/module-graph-resolver.ts
@@ -1,21 +1,25 @@
 /**
  * Extracts and resolves dependency requests of one or more assets, shallow or deep.
- *
- * @param assetKey unique key(s) of asset(s) to resolve dependency requests for.
- * @param deep should process follow resolved requests, and resolve these assets as well (defaults to `false`).
- * @returns record containing asset keys as fields, and their resolved dependecy requests as values.
+ * @param filePath file to resolve dependency requests for.
  */
-export type DependencyResolver = (assetKey: string | string[]) => Promise<ModuleGraph>;
+export type ModuleGraphResolver = (entryFilePaths: string | string[]) => Promise<ModuleGraph>;
 
+/**
+ * Map containing file paths as keys, alongside their compiled contents and resolved dependecy requests.
+ */
 export type ModuleGraph = Map<string, ModuleGraphNode>;
 export type ModuleGraphNode = { compiledContents: string; resolvedRequests: ResolvedRequests };
 
 /**
  * Map containing requests as fields, and their resolutions as values.
- * `undefined` is used when a request couldn't be resolved.
  */
 export type ResolvedRequests = Map<string, SpecifierResolution>;
 
+/**
+ * - `string` for a resolved file path
+ * - `false` should map to an empty object in runtime
+ * - `undefined` couldn't be resolved
+ */
 export type SpecifierResolution = string | false | undefined;
 
 export interface AnalyzedModule {
@@ -27,7 +31,7 @@ export interface IDependencyResolverOptions {
   /**
    * Extracts a dependency requests list of an asset.
    *
-   * @param assetKey unique identifier for an asset to extract from.
+   * @param filePath unique identifier for an asset to extract from.
    * @returns list of dependency requests by the asset.
    */
   analyzeModule(filePath: string): Promise<AnalyzedModule>;
@@ -35,7 +39,7 @@ export interface IDependencyResolverOptions {
   /**
    * Resolve a dependency request by an asset.
    *
-   * @param assetKey unique identifier of the requesting asset.
+   * @param filePath unique identifier of the requesting asset.
    * @returns unique key for the asset the request resolves to.
    */
   resolveRequest: (filePath: string, request: string) => SpecifierResolution | Promise<SpecifierResolution>;
@@ -48,36 +52,44 @@ export interface IDependencyResolverOptions {
 export function createModuleGraphResolver({
   analyzeModule,
   resolveRequest,
-}: IDependencyResolverOptions): DependencyResolver {
-  return async (assetKey) => {
-    const resolvedAssets = new Map<string, ModuleGraphNode>();
-    const assetsToResolve: string[] = Array.isArray(assetKey) ? [...assetKey] : [assetKey];
+}: IDependencyResolverOptions): ModuleGraphResolver {
+  const analyzeAndResolve = async (filePath: string): Promise<[string, ModuleGraphNode]> => {
+    const { compiledContents, requests } = await analyzeModule(filePath);
+    const resolvedRequests: ResolvedRequests = new Map();
 
-    while (assetsToResolve.length > 0) {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const currentAsset = assetsToResolve.shift()!;
-
-      if (resolvedAssets.has(currentAsset)) {
-        continue;
+    for (const request of requests) {
+      if (resolvedRequests.has(request)) {
+        continue; // already resolved this request
       }
+      const resolvedRequest = await resolveRequest(filePath, request);
+      resolvedRequests.set(request, resolvedRequest);
+    }
+    return [filePath, { compiledContents, resolvedRequests }];
+  };
 
-      const { compiledContents, requests } = await analyzeModule(currentAsset);
-      const resolvedRequests: ResolvedRequests = new Map();
-      resolvedAssets.set(currentAsset, { compiledContents, resolvedRequests });
+  return async (entryFilePaths) => {
+    const moduleGraph = new Map<string, ModuleGraphNode>();
 
-      for (const request of requests) {
-        if (resolvedRequests.has(request)) {
-          continue; // already resolved this request
-        }
+    entryFilePaths = Array.isArray(entryFilePaths) ? entryFilePaths : [entryFilePaths];
+    const workInProgress = new Map<string, Promise<[string, ModuleGraphNode]>>(
+      entryFilePaths.map((filePath) => [filePath, analyzeAndResolve(filePath)]),
+    );
 
-        const resolvedRequest = await resolveRequest(currentAsset, request);
-        resolvedRequests.set(request, resolvedRequest);
-        if (resolvedRequest !== undefined && resolvedRequest !== false) {
-          assetsToResolve.push(resolvedRequest);
+    while (workInProgress.size > 0) {
+      const [filePath, graphNode] = await Promise.race(workInProgress.values());
+      workInProgress.delete(filePath);
+      moduleGraph.set(filePath, graphNode);
+      for (const resolvedFilePath of graphNode.resolvedRequests.values()) {
+        if (
+          typeof resolvedFilePath === "string" &&
+          !moduleGraph.has(resolvedFilePath) &&
+          !workInProgress.has(resolvedFilePath)
+        ) {
+          workInProgress.set(resolvedFilePath, analyzeAndResolve(resolvedFilePath));
         }
       }
     }
 
-    return resolvedAssets;
+    return moduleGraph;
   };
 }
