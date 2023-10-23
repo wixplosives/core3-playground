f37e2e68378e1af5254be680290cde39d20a2f1e
Avi Vahl <avi.vahl@wix.com>
2023-10-24 01:32:39 +0300

    chore(deps-dev): sass@1.69.4, latest others - bump fallback sass version as well - regenerate lock file

--- src/constants.ts
+++ src/constants.ts
@@ -13,7 +13,7 @@ export const compilationBundleName = "compilation-worker.js";
 export const defaultCompilationWorkerCount = 4;
 export const defaultLibVersions: LibraryVersions = {
   typescript: tsVersion,
-  sass: "1.69.0", // sassVersion
+  sass: "1.69.4", // sassVersion
   immutable: immutableVersion,
 };
 export const previewIframeHTMLIndex = "preview.html";
