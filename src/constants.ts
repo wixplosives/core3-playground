c61de04a77f653fa2287528067fdf96cbecbd97c
Avi Vahl <avi@vahl.co.il>
2024-06-19 19:16:50 +0300

    feat: use sass@1.77.6 as default fallback

--- src/constants.ts
+++ src/constants.ts
@@ -13,7 +13,7 @@ export const compilationBundleName = "compilation-worker.js";
 export const defaultCompilationWorkerCount = 4;
 export const defaultLibVersions: LibraryVersions = {
   typescript: tsVersion,
-  sass: "1.77.4", // sassVersion
+  sass: "1.77.6", // sassVersion
   immutable: immutableVersion,
 };
 export const previewIframeHTMLIndex = "preview.html";
