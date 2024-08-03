46db20b13f4bbf8d12133715f9ee84c98dbe8b8d
Avi Vahl <avi@vahl.co.il>
2024-08-04 02:03:19 +0300

    infra: migrate to eslint@9

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import pluginJs from "@eslint/js";
+import configPrettier from "eslint-config-prettier";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginTypescript from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ["dist/", "**/*.{js,mjs,cjs}"] },
+  pluginJs.configs.recommended,
+
+  ...pluginTypescript.configs.recommendedTypeChecked,
+  { languageOptions: { parserOptions: { projectService: true } } },
+
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  { settings: { react: { version: "detect" } } },
+
+  { plugins: { "react-hooks": pluginReactHooks } },
+  configPrettier,
+
+  {
+    rules: {
+      "no-console": "error",
+      "react/prop-types": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "error",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "@typescript-eslint/unbound-method": "off",
+    },
+  },
+];
