8ae7d63679fc257e3d8435ada86d3ab6ee81119a
Avi Vahl <avi@vahl.co.il>
2024-09-20 15:00:27 +0300

    chore(deps-dev): sass@1.79.2 https://github.com/sass/dart-sass/releases/tag/1.79.2

--- src/constants.ts
+++ src/constants.ts
@@ -13,7 +13,7 @@ export const compilationBundleName = "compilation-worker.js";
 export const defaultCompilationWorkerCount = 4;
 export const defaultLibVersions: LibraryVersions = {
   typescript: tsVersion,
-  sass: "1.79.1", // sassVersion
+  sass: "1.79.2", // sassVersion
   immutable: immutableVersion,
 };
 export const previewIframeHTMLIndex = "preview.html";
