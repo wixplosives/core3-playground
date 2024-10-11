7a63a6069816f97a9a73b1dc76e0fafea1cb8c09
Avi Vahl <avi@vahl.co.il>
2024-10-11 12:33:05 +0300

    chore(deps-dev): sass@1.79.5 and regenerate lock file

--- src/constants.ts
+++ src/constants.ts
@@ -13,7 +13,7 @@ export const compilationBundleName = "compilation-worker.js";
 export const defaultCompilationWorkerCount = 4;
 export const defaultLibVersions: LibraryVersions = {
   typescript: tsVersion,
-  sass: "1.79.4", // sassVersion
+  sass: "1.79.5", // sassVersion
   immutable: immutableVersion,
 };
 export const previewIframeHTMLIndex = "preview.html";
