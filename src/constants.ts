0b36ada1cdd48fb30add4bc897b04040723401b0
Avi Vahl <avi@vahl.co.il>
2024-10-30 00:17:46 +0200

    chore(deps-dev): upgrade to latest stable and regenerate lock file

--- src/constants.ts
+++ src/constants.ts
@@ -13,7 +13,7 @@ export const compilationBundleName = "compilation-worker.js";
 export const defaultCompilationWorkerCount = 4;
 export const defaultLibVersions: LibraryVersions = {
   typescript: tsVersion,
-  sass: "1.80.4", // sassVersion
+  sass: "1.80.5", // sassVersion
   immutable: immutableVersion,
 };
 export const previewIframeHTMLIndex = "preview.html";
