ff04374fb8e04eb1e43f40c2e9ca1fefa3475071
Avi Vahl <avi@vahl.co.il>
2024-11-18 01:45:06 +0200

    feat: integrate react-devtools-inline wip

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
+import type { DevtoolsProps } from "react-devtools-inline/frontend";
 import { previewIframeHTMLIndex } from "../constants";
 import { classNames } from "../helpers/dom";
 import classes from "./preview.module.css";
@@ -9,49 +10,55 @@ export namespace Preview {
   export interface Props {
     className?: string | undefined;
     filePath: string;
+    DevTools?: React.ComponentType<DevtoolsProps> | undefined;
     onPreviewLoad?: ((filePath: string, iframe: HTMLIFrameElement) => unknown) | undefined;
     onPreviewClose?: ((filePath: string) => unknown) | undefined;
   }
 }
 
-export const Preview: React.FC<Preview.Props> = React.memo(({ className, filePath, onPreviewLoad, onPreviewClose }) => {
-  const iframeRef = useRef<HTMLIFrameElement>(null);
-  const [iframeSrc, setIframeSrc] = useState(previewIframeHTMLIndex);
+export const Preview: React.FC<Preview.Props> = React.memo(
+  ({ className, filePath, onPreviewLoad, onPreviewClose, DevTools }) => {
+    const iframeRef = useRef<HTMLIFrameElement>(null);
+    const [iframeSrc, setIframeSrc] = useState(previewIframeHTMLIndex);
 
-  const onReloadClick = () => {
-    setIframeSrc(aboutBlank);
-  };
+    const onReloadClick = () => {
+      setIframeSrc(aboutBlank);
+    };
 
-  const onLoad: React.ReactEventHandler<HTMLIFrameElement> = ({ currentTarget }) => {
-    if (iframeSrc === aboutBlank) {
-      setIframeSrc(previewIframeHTMLIndex);
-    } else {
-      onPreviewLoad?.(filePath, currentTarget);
-    }
-  };
+    const onLoad: React.ReactEventHandler<HTMLIFrameElement> = ({ currentTarget }) => {
+      if (iframeSrc === aboutBlank) {
+        setIframeSrc(previewIframeHTMLIndex);
+      } else {
+        onPreviewLoad?.(filePath, currentTarget);
+      }
+    };
 
-  useEffect(
-    () => () => {
-      onPreviewClose?.(filePath);
-    },
-    [filePath, onPreviewClose],
-  );
+    useEffect(
+      () => () => {
+        onPreviewClose?.(filePath);
+      },
+      [filePath, onPreviewClose],
+    );
 
-  return (
-    <div className={classNames(classes["preview"], className)}>
-      <button className={classes["reloadButton"]} onClick={onReloadClick}>
-        Reload
-      </button>
-      <iframe
-        className={classes["previewIframe"]}
-        src={iframeSrc}
-        ref={iframeRef}
-        sandbox="allow-scripts allow-popups allow-modals"
-        data-file-path={filePath}
-        onLoad={onLoad}
-      />
-    </div>
-  );
-});
+    return (
+      <div className={classNames(classes["preview"], className)}>
+        <button className={classes["reloadButton"]} onClick={onReloadClick}>
+          Reload
+        </button>
+        <div className={classes["sideBySide"]}>
+          <iframe
+            className={classes["previewIframe"]}
+            src={iframeSrc}
+            ref={iframeRef}
+            sandbox="allow-scripts allow-popups allow-modals"
+            data-file-path={filePath}
+            onLoad={onLoad}
+          />
+          {DevTools && <DevTools browserTheme="dark" />}
+        </div>
+      </div>
+    );
+  },
+);
 
 Preview.displayName = "Preview";
