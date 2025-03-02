edf78177c89cf94e7742fc877daad46dd210a8e3
Avi Vahl <avi@vahl.co.il>
2025-03-02 06:33:53 +0200

    fix: disable sandbox on preview iframe esbuild stopped setting loose CORS headers on development server, and iframe loads our chosen code anyway

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -46,7 +46,6 @@ export const Preview: React.FC<Preview.Props> = React.memo(({ className, filePat
         className={classes["previewIframe"]}
         src={iframeSrc}
         ref={iframeRef}
-        sandbox="allow-scripts allow-popups allow-modals"
         data-file-path={filePath}
         onLoad={onLoad}
       />
