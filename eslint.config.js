0df19791ba6d54749d66fe542f6c190d29c5aa55
Avi Vahl <avi.vahl@wix.com>
2025-03-16 11:07:35 +0200

    feat: remove eslint-plugin-react accounted for many dependencies in repository. some rules only make sense for older react and class components. others only make sense in a non-typescript project. some are already covered by react warnings. eslint-plugin-react-hooks is still installed and used.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,5 @@
 import pluginJs from "@eslint/js";
 import configPrettier from "eslint-config-prettier";
-import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginTypescript from "typescript-eslint";
 
@@ -12,17 +11,12 @@ export default [
   ...pluginTypescript.configs.recommendedTypeChecked,
   { languageOptions: { parserOptions: { projectService: true } } },
 
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat["jsx-runtime"],
-  { settings: { react: { version: "detect" } } },
-
   { plugins: { "react-hooks": pluginReactHooks } },
   configPrettier,
 
   {
     rules: {
       "no-console": "error",
-      "react/prop-types": "off",
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "error",
       "@typescript-eslint/no-explicit-any": "off",
